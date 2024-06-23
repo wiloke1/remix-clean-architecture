78128f974e934f46a68c218e66b94e4a9c12d66a
Nguyen Long <hpvanlong@gmail.com>
2024-06-23 11:45:19 +0700

    update create loader

--- app/libs/utils/create-loader.tsx
+++ app/libs/utils/create-loader.tsx
@@ -1,4 +1,5 @@
 import type { LoaderFunctionArgs, SerializeFrom } from '@remix-run/node';
+import type { Location } from '@remix-run/react';
 import { Await, defer, json, useLoaderData, useNavigation } from '@remix-run/react';
 import type { ReactNode } from 'react';
 import { Suspense, useEffect, useState } from 'react';
@@ -17,18 +18,35 @@ export interface MatchConsumerProps<DataT extends SerializeFrom<Record<string, a
 
 export interface RouteConsumerProps<DataT extends SerializeFrom<Record<string, any>>> extends MatchConsumerProps<DataT> {}
 
-function useDataPrivate<DataT extends SerializeFrom<Record<string, any>>>(callback: Callback<DataT>, prevData: DataT | null, isJson = false) {
+class Cache extends Map {
+  public get<DataT extends Record<string, any>>(args: LoaderFunctionArgs) {
+    return super.get(JSON.stringify(args)) as DataT;
+  }
+
+  public set<DataT extends Record<string, any>>(args: LoaderFunctionArgs, data: DataT) {
+    return super.set(JSON.stringify(args), data);
+  }
+
+  public has(args: LoaderFunctionArgs) {
+    return super.has(JSON.stringify(args));
+  }
+}
+
+let prevLocationFromNavigation: Location | undefined;
+
+function useDataPrivate<DataT extends SerializeFrom<Record<string, any>>>(callback: Callback<DataT>, cache: Cache, isJson = false) {
   const { data, args } = useLoaderData<DefaultLoaderType>();
-  const [dataState, setDataState] = useState<DataT>(prevData ?? data);
+  const [dataState, setDataState] = useState<DataT>(cache.get(args) ?? data);
   const [needFetch, setNeedFetch] = useState(false);
   const navigation = useNavigation();
 
   useEffect(() => {
-    setNeedFetch(navigation.state !== 'loading');
-  }, [navigation.state]);
+    setNeedFetch(navigation.state !== 'loading' && !!prevLocationFromNavigation);
+    prevLocationFromNavigation = navigation.location;
+  }, [navigation]);
 
   useEffect(() => {
-    if (needFetch) {
+    if (needFetch && args) {
       if (isJson) {
         callback(args).then(setDataState).catch(console.error);
       } else {
@@ -38,44 +56,39 @@ function useDataPrivate<DataT extends SerializeFrom<Record<string, any>>>(callba
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [needFetch, args]);
 
-  return dataState as DataT;
-}
-
-function useConsumerDataPrivate<DataT extends SerializeFrom<Record<string, any>>>(callback: Callback<DataT>, prevData: DataT | null) {
-  const { data, args } = useLoaderData<DefaultLoaderType>();
-  const [dataState, setDataState] = useState<DataT>(prevData ?? data);
+  useEffect(() => {
+    setDataState(data);
+  }, [data]);
 
   useEffect(() => {
-    if (callback) {
-      setDataState(callback(args));
+    if (args) {
+      cache.set(args, dataState);
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [dataState, args]);
 
-  return { data: dataState, isLoading: !args };
+  return { data: dataState, args };
 }
 
 export function createDeferLoader<DataT extends Record<string, any>>(callback: Callback<DataT>, init?: number | ResponseInit) {
-  let cache: DataT | null = null;
+  let cache = new Cache();
 
   function loader(args: LoaderFunctionArgs) {
-    if (cache) {
-      return defer({ data: cache, args }, init);
+    if (cache.has(args)) {
+      return defer({ data: cache.get(args), args }, init);
     }
     const data = callback(args);
-    cache = data;
+    cache.set(args, data);
     return defer({ data }, init);
   }
 
   function useData() {
-    const data = useDataPrivate<Promise<SerializeFrom<DataT>>>(callback as any, cache as any);
-    cache = data as any as DataT;
+    const { data } = useDataPrivate<Promise<SerializeFrom<DataT>>>(callback as any, cache as any);
     return data;
   }
 
   function useConsumerData() {
-    const data = useConsumerDataPrivate<Promise<SerializeFrom<DataT>>>(callback as any, cache as any);
-    cache = data.data as any as DataT;
+    const data = useDataPrivate<Promise<SerializeFrom<DataT>>>(callback as any, cache);
     return data;
   }
 
@@ -83,10 +96,15 @@ export function createDeferLoader<DataT extends Record<string, any>>(callback: C
     return cache;
   }
 
-  function Consumer({ fallback = null, children }: ConsumerProps<SerializeFrom<DataT>>) {
-    const { data, isLoading } = useConsumerData();
+  function clearCache() {
+    cache.clear();
+  }
 
-    if (isLoading) {
+  function Consumer({ fallback = null, children }: ConsumerProps<SerializeFrom<DataT>>) {
+    const { data, args } = useConsumerData();
+    const hasLoading = !args;
+
+    if (hasLoading) {
       return (
         <Suspense fallback={fallback}>
           <Await resolve={data}>{children as any}</Await>
@@ -109,26 +127,26 @@ export function createDeferLoader<DataT extends Record<string, any>>(callback: C
     loader,
     useData,
     useCacheData,
+    clearCache,
     Consumer,
     CacheConsumer,
   };
 }
 
 export function createJsonLoader<DataT extends Record<string, any>>(callback: Callback<Promise<DataT>>, init?: number | ResponseInit) {
-  let cache: DataT | null = null;
+  let cache = new Cache();
 
   async function loader(args: LoaderFunctionArgs) {
-    if (cache) {
-      return json({ data: cache }, init);
+    if (cache.has(args)) {
+      return json({ data: cache.get(args), args }, init);
     }
     const data = await callback(args);
-    cache = data;
+    cache.set(args, data);
     return json({ data }, init);
   }
 
   function useData() {
-    const data = useDataPrivate<SerializeFrom<DataT>>(callback as any, cache as any, true);
-    cache = data as any as DataT;
+    const { data } = useDataPrivate<SerializeFrom<DataT>>(callback as any, cache, true);
     return data;
   }
 
@@ -136,9 +154,14 @@ export function createJsonLoader<DataT extends Record<string, any>>(callback: Ca
     return cache;
   }
 
+  function clearCache() {
+    cache.clear();
+  }
+
   return {
     loader,
     useData,
     useCacheData,
+    clearCache,
   };
 }
